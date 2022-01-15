46f8fe3798f4f8142276171f88c8f34144eff4b0
carlos-hernandez1993 <carlosluis220993@gmail.com>
2022-01-15 13:01:14 +0100

    funciones

--- utils/functions.py
+++ utils/functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy.io import arff
+import re
 
 def getProcessedDataFrame(filepath):
   dataset = arff.loadarff(filepath)
@@ -26,3 +27,36 @@ def convertEncodingToPositive(dataframe):
     #   print(i)
     for j in range(dataframe.shape[1]):
       dataframe.loc[i][j] = mapping[dataframe.loc[i][j]]
+
+def doble_barra(url):
+  '''The existence of “//” within the URL path. If returns 1 is legitimate, if returns 2 is phishing'''
+  if ((url)[5] == "/" and url[6] == "/") or (url[6] == "/" and url[7] == "/"):
+      n=1
+  else:
+      n=2
+  return n
+
+def guion(url):
+  '''The existence of "-" whithin the URL path.If returns 1 is legitimate, if returns 2 is phishing'''
+  if "-" in url:
+      n=2
+  else:
+      n=1
+  return n
+
+def busqueda_ip(url):
+  '''The existence of ip whithin the URL path.If returns 1 is legitimate, if returns 2 is phishing'''
+  pattern = re.compile(r'https?:\/\/.[\d]{3}.[\d]{2}.[\d]{3}.[\d]{3}') #busqueda de ruta con numeros
+  if pattern.findall(url):
+      n=2
+  else:
+      n=1
+  return n
+
+def arroba(url):
+  '''The existence of "-" whithin the URL path.If returns 1 is legitimate, if returns 2 is phishing'''
+  if '@' in url:
+      n=2
+  else:
+      n=1
+  return n
